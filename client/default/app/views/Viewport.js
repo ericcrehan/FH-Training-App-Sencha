61b2370c609c5f24dfd5e5b0748221881071b0cd
eric.crehan@feedhenry.com <eric.crehan@feedhenry.com>
2012-08-14 11:31:04 +0000

    update file /client/default/app/views/Viewport.js

--- client/default/app/views/Viewport.js
+++ client/default/app/views/Viewport.js
@@ -11,24 +11,14 @@ app.views.Viewport = Ext.extend(Ext.Panel, {
   initComponent: function() {
     // Put instances of cards into app.views namespace
     Ext.apply(app.views, {
-      home:     new app.views.Home(),
-      map:      new app.views.MapView(),
-      twitter:  new app.views.Twitter(),        
-      payment:  new app.views.Payment(),
-      stocks:   new app.views.Stocks(),
-      settings: new app.views.Settings(),
-      camera:   new app.views.Camera()
+     
     });
-    //put instances of cards into viewport
+    //put instances of cards (views) into viewport
     Ext.apply(this, {
       items: [
-        app.views.home,
-        app.views.payment,
-        app.views.stocks,
-        app.views.twitter,
-        app.views.map,
-        app.views.settings,
-        app.views.camera
+       {
+       html: 'Test View'
+     }
       ]
     });
     app.views.Viewport.superclass.initComponent.apply(this, arguments);
